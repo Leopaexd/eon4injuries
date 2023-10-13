de44143001a684b5c13f067826c40b27a9544563
Leopaexd <oliver.glant@gmail.com>
2023-10-13 13:02:15 +0200

    Made test verbose

--- test_injuries.py
+++ test_injuries.py
@@ -6,6 +6,6 @@ from injuries import InjuryRoller
 class TestInjuryRoller(TestCase):
     def test_injury_effects(self):
         injuryroller = InjuryRoller()
-        roll1 = injuryroller.injury_effects(13, 'Huggskada mot huvud', +1, verbose=False)
+        roll1 = injuryroller.injury_effects(13, 'Slagsmålsskada mot huvud', 1, verbose=True)
         print(roll1)
         self.assertEqual(len(roll1), 3, msg='Injury result is not a list of length 3')
